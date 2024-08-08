7e27ebb13441baf69b0d6cf513fade750de7d161
dps-broken <107934635+dps-broken@users.noreply.github.com>
2024-08-08 12:16:46 +0530

    "Updated HTML, CSS, and JavaScript files with various changes to layout, styling, and functionality."

--- script.js
+++ script.js
@@ -67,10 +67,11 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolled');
     }
 });
-
 // Toggle menu for mobile view
 const navToggle = document.querySelector('.nav-toggle');
 const navContainer = document.querySelector('.nav-container');
+
 navToggle.addEventListener('click', () => {
     navContainer.classList.toggle('active');
+    navToggle.classList.toggle('active');
 });
