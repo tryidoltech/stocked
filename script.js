3bdf5db64f12935090d954065b2ebe329f85b49b
dps-broken <107934635+dps-broken@users.noreply.github.com>
2024-08-08 17:40:16 +0530

    nn

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ setInterval(nextSlide, 5000);
 showSlide(currentSlide);
 
 // Navbar scroll behavior
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 50) {
         navbar.classList.add('scrolled');
@@ -75,5 +75,18 @@ navToggle.addEventListener('click', () => {
     navContainer.classList.toggle('active');
     navToggle.classList.toggle('active');
 });
+// faq
 
 
+document.querySelectorAll('.faq-item-header').forEach(item => {
+    item.addEventListener('click', () => {
+        const faqItem = item.parentElement;
+        faqItem.classList.toggle('active');
+        const icon = item.querySelector('span');
+        if (faqItem.classList.contains('active')) {
+            icon.textContent = '-';
+        } else {
+            icon.textContent = '+';
+        }
+    });
+});
