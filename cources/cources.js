a9de4318f32f4b2d6141bb1530e5b4cbd15f6647
dps-broken <107934635+dps-broken@users.noreply.github.com>
2024-08-28 10:58:10 +0530

    "Updated navigation menu links and added banner section to FAQ page, also modified CSS for contact and FAQ pages."

---
+++ cources/cources.js
@@ -0,0 +1,24 @@
+
+// Toggle menu for mobile view
+const navToggle = document.querySelector('.nav-toggle');
+const navContainer = document.querySelector('.nav-container');
+
+navToggle.addEventListener('click', () => {
+    navContainer.classList.toggle('active');
+    navToggle.classList.toggle('active');
+});
+// faq
+
+
+document.querySelectorAll('.faq-item-header').forEach(item => {
+    item.addEventListener('click', () => {
+        const faqItem = item.parentElement;
+        faqItem.classList.toggle('active');
+        const icon = item.querySelector('span');
+        if (faqItem.classList.contains('active')) {
+            icon.textContent = '-';
+        } else {
+            icon.textContent = '+';
+        }
+    });
+});
